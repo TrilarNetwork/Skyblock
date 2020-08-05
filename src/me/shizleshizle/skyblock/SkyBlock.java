3af96b61d4d2e18ed8ebe493349678621c9f7181
LenDR1 <lenneth.00@hotmail.com>
2020-08-05 10:43:39 +0200

    SkyBlock Setup
    Klassen aanmaken

--- src/me/shizleshizle/skyblock/SkyBlock.java
+++ src/me/shizleshizle/skyblock/SkyBlock.java
@@ -1,7 +1,11 @@
 package me.shizleshizle.skyblock;
 
-import me.shizleshizle.skyblock.objects.IslandHandler;
 import me.shizleshizle.core.mysql.MySQLManager;
+import me.shizleshizle.skyblock.commands.ClaimCommand;
+import me.shizleshizle.skyblock.events.ClaimEvent;
+import me.shizleshizle.skyblock.events.MiningEvent;
+import me.shizleshizle.skyblock.events.SkillEvent;
+import me.shizleshizle.skyblock.objects.IslandHandler;
 import me.shizleshizle.skyblock.utils.BlockMetaData;
 import me.shizleshizle.skyblock.utils.Claims;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,6 +22,11 @@ public class SkyBlock extends JavaPlugin {
     public ArrayList<BlockMetaData> claimPoints = new ArrayList<BlockMetaData>();
     public ArrayList<Claims> claims = new ArrayList<Claims>();
 
+    public SkillEvent skillEvent;
+    public MiningEvent miningEvent;
+    public ClaimEvent claimEvent;
+    public Claims claim;
+
     public void onEnable() {
         final long time = System.currentTimeMillis();
         getLogger().info("Skyblock >> Enabling...");
@@ -29,6 +38,7 @@ public class SkyBlock extends JavaPlugin {
         islands.setup();
         final long result = System.currentTimeMillis() - time;
         getLogger().info("Skyblock >> Enabled! (" + result + " ms)");
+        skyblockSetup();
     }
 
     public void onDisable() {
@@ -43,5 +53,16 @@ public class SkyBlock extends JavaPlugin {
         }
         final long result = System.currentTimeMillis() - time;
         getLogger().info("Skyblock >> Disabled! (" + result + " ms)");
+    }
+
+    private void skyblockSetup(){
+        getCommand("claim").setExecutor(new ClaimCommand(this));
+        getServer().getPluginManager().registerEvents(new MiningEvent(this), this);
+        getServer().getPluginManager().registerEvents(new ClaimEvent(this), this);
+        skillEvent = new SkillEvent(this);
+        miningEvent = new MiningEvent(this);
+        claimEvent = new ClaimEvent(this);
+        claim = new Claims(this);
+        skillEvent.runBlockChanger();
     }
 }
